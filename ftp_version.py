b6f86e9316f452de1d61dbeb59165b640945572b
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-04-17 19:29:13 -0300

    Update ftp_version.py

--- ftp_version.py
+++ ftp_version.py
@@ -1,1 +1,30 @@
-#ira fazer o ftp
+import requests
+import re
+
+# find and save the current Github release
+html = str(
+    requests.get('https://github.com/containerd/containerd/releases/latest')
+    .content)
+index = html.find('Release ')
+github_version = html[index + 19:index + 24]
+file = open('github_version.txt', 'w')
+file.writelines(github_version)
+file.close()
+
+# find and save the current Bazel version on FTP server
+html = str(
+    requests.get(
+        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/containerd-cri/'
+    ).content)
+index = html.rfind('containerd-cri-')
+ftp_version = html[index + 15:index + 21]
+file = open('ftp_version.txt', 'w')
+file.writelines(ftp_version)
+file.close()
+
+# find and save the oldest Bazel version on FTP server
+index = html.find('containerd-cri-')
+delete = html[index + 15:index + 21]
+file = open('delete_version.txt', 'w')
+file.writelines(delete)
+file.close()
