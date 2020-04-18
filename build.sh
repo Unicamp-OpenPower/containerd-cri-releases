34a5931fe7949542bbc1ff87eb7ff4698af58f85
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-04-18 01:32:04 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ del_version=$(cat delete_version.txt)
 
 if [ $github_version != $ftp_version ]
 then
-  python3 no_space.py
   export GOPATH=/var/lib/jenkins/workspace/containerd-cri-release
   #cd /usr/local/go/src/
   echo "Criando pastas"
