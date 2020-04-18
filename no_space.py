2c22b08f24f9e5e0fcf93c83cec553701406ce2f
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-04-18 01:32:36 -0300

    Update and rename no_space.py to validation.py

--- no_space.py
+++
@@ -1,8 +0,0 @@
-file = open('github_version.txt', 'r')
-version = file.readline()
-version = version.strip()
-file.close()
-
-file = open('github_version.txt', 'w')
-file.writelines(version)
-file.close()
